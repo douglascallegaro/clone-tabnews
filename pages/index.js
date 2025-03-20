58ab3dd6a90904005f8530f88bfd6cc6361bdd8e
douglascallegaro <douglas.callegaro@gmail.com>
2025-03-20 20:47:08 +0000

    ajuste de erro do index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1> Primeiros momentos no GitHub e Vercel, acho que fiz um bom investimneto. </h1>
+    return <h1> Primeiros momentos no GitHub e Vercel, acho que fiz um bom investimneto.. </h1>
 }
 
 export default Home;
